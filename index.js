e20e441f5ea2de6fa40a6743ab890916b50d7968
Zachary Clinton Jones <zajonesck83@gmail.com>
2017-12-13 15:46:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-function SayHiToGrandma(string) {
+function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
